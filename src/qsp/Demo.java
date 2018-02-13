624d6a5f2a98cf0f56b48ee2b5618c48965b675b
SBRakesh <rakeshsb313@gmail.com>
2018-02-13 09:07:56 +0530

    first commit

---
+++ src/qsp/Demo.java
@@ -0,0 +1,19 @@
+package qsp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void testA()
+	{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		Reporter.log(driver.getTitle(),true);
+		driver.close();
+	}
+}
